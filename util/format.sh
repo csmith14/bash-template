719c163355e9e732ce51c41de831d8fc29413879
Cam Smith <csmith14@protonmail.com>
2021-05-18 21:39:44 -0700

    Add flag explanations

--- util/format.sh
+++ util/format.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-shfmt -w -i 2 -ci -l template-v1
+# -w    write result to file
+# -i 2  indent 2 spaces
+# -ci   indent switch cases
+# -l    list unformatted
+
+shfmt -w -i 2 -ci -l template-v1 lib/message-lib.sh lib/usage-lib.sh && echo '
+All files formatted!
+'
